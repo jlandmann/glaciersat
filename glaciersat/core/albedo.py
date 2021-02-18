da98031f5838fcaaaec5f1888229f1e8fc31f83a
Johannes Landmann <j_landmann@gmx.de>
2021-02-18 14:59:41 +0100

    formatting and clips

--- glaciersat/core/albedo.py
+++ glaciersat/core/albedo.py
@@ -1,11 +1,14 @@
 import numpy as np
 import xarray as xr
 import pandas as pd
+from typing import Union
 
 xr.set_options(keep_attrs=True)
 
 
-def get_broadband_albedo_knap(g, nir):
+def get_broadband_albedo_knap(
+        g: Union[float, np.array],
+        nir: Union[float, np.array]) -> Union[float, np.array]:
     """
     Get broad band albedo after Knap (1999) ([1]_) as used in [2]_.
 
@@ -22,7 +25,7 @@ def get_broadband_albedo_knap(g, nir):
 
     Returns
     -------
-    np.array:
+    same as input:
         Broad band albedo after [1]_.
 
     References
@@ -36,12 +39,17 @@ def get_broadband_albedo_knap(g, nir):
         Remote Sensing, 2017, 9.
     """
 
-    return 0.726 * g - 0.322 * g ** 2 - 0.015 * nir + 0.581 * nir ** 2
+    alpha = 0.726 * g - 0.322 * g ** 2 - 0.015 * nir + 0.581 * nir ** 2
+
+    return np.clip(alpha, 0., 1.)
 
 
-def get_broadband_albedo_liang(b: np.ndarray, r: np.ndarray, nir: np.ndarray,
-                               swir1: np.ndarray, swir2: np.ndarray) \
-        -> np.ndarray:
+def get_broadband_albedo_liang(
+        b: Union[float, np.array],
+        r: Union[float, np.array],
+        nir: Union[float, np.array],
+        swir1: Union[float, np.array],
+        swir2: Union[float, np.array]) -> Union[float, np.array]:
     """
     Get broad band albedo after Liang (2001) ([1]_) as used in [2]_.
 
@@ -51,15 +59,15 @@ def get_broadband_albedo_liang(b: np.ndarray, r: np.ndarray, nir: np.ndarray,
 
     Parameters
     ----------
-    b: np.array
+    b: float or np.array
         Blue band.
-    r: np.array
+    r: float or np.array
         Red band.
-    nir: np.array
+    nir: float or np.array
         Near-infrared band.
-    swir1: np.array
+    swir1: float or np.array
         Short wave-infrared band 1.
-    swir2: np.array
+    swir2: float or np.array
         Short wave-infrared band 2.
 
     Returns
@@ -77,13 +85,18 @@ def get_broadband_albedo_liang(b: np.ndarray, r: np.ndarray, nir: np.ndarray,
         from Airborne and Satellite (Sentinel-2 and Landsat 8) Optical Data
         Remote Sensing, 2017, 9.
     """
-    return 0.356 * b + 0.130 * r + 0.373 * nir + 0.085 * swir1 + 0.072 * \
-           swir2 - 0.0018
+    alpha = 0.356 * b + 0.130 * r + 0.373 * nir + 0.085 * swir1 + 0.072 * \
+            swir2 - 0.0018
+    return np.clip(alpha, 0., 1.)
 
 
-def get_broadband_albedo_bonafoni(b: np.ndarray, g: np.ndarray, r: np.ndarray,
-                                  nir: np.ndarray, swir1: np.ndarray,
-                                  swir2: np.ndarray) -> np.ndarray:
+def get_broadband_albedo_bonafoni(
+        b: Union[float, np.array],
+        g: Union[float, np.array],
+        r: Union[float, np.array],
+        nir: Union[float, np.array],
+        swir1: Union[float, np.array],
+        swir2: Union[float, np.array]) -> Union[float, np.array]:
     """
     Get broad band albedo after Bonafoni (2020) ([1]_).
 
@@ -93,22 +106,22 @@ def get_broadband_albedo_bonafoni(b: np.ndarray, g: np.ndarray, r: np.ndarray,
 
     Parameters
     ----------
-    b: np.array
+    b: float or np.array
         Blue band.
-    g: np.array
+    g: float or np.array
         Green band.
-    r: np.array
+    r: float or np.array
         Red band.
-    nir: np.array
+    nir: float or np.array
         Near-infrared band.
-    swir1: np.array
+    swir1: float or np.array
         Short wave-infrared band 1.
-    swir2: np.array
+    swir2: float or np.array
         Short wave-infrared band 2.
 
     Returns
     -------
-    np.array:
+    same as input:
         Broad band albedo after [1]_.
 
     References
@@ -118,12 +131,17 @@ def get_broadband_albedo_bonafoni(b: np.ndarray, g: np.ndarray, r: np.ndarray,
         Geoscience and Remote Sensing Letters.
     """
 
-    return b * 0.2266 + g * 0.1236 + r * 0.1573 + nir * 0.3417 + \
-           swir1 * 0.1170 + swir2 * 0.0338
+    alpha = b * 0.2266 + g * 0.1236 + r * 0.1573 + nir * 0.3417 + \
+            swir1 * 0.1170 + swir2 * 0.0338
+
+    # just to be sure...
+    return np.clip(alpha, 0., 1.)
 
 
-def get_proxy_albedo_mccarthy(r: np.ndarray, g: np.ndarray, b: np.ndarray) \
-        -> np.ndarray:
+def get_proxy_albedo_mccarthy(
+        r: Union[float, np.array],
+        g: Union[float, np.array],
+        b: Union[float, np.array]) -> Union[float, np.array]:
     """
     Get a 'proxy albedo' (sum of RGB) as discussed with D. Rounce in McCarthy.
 
@@ -131,22 +149,28 @@ def get_proxy_albedo_mccarthy(r: np.ndarray, g: np.ndarray, b: np.ndarray) \
 
     Parameters
     ----------
-    r: np.ndarray
+    r: float or np.ndarray
         Red band.
-    g: np.ndarray
+    g: float or np.ndarray
         Green band.
-    b: np.ndarray
+    b: float or np.ndarray
         Blue band.
 
     Returns
     -------
-    np.ndarray
+    float or np.ndarray
         Array with proxy for albedo.
     """
     return (r + g + b) / 3.
 
 
-def get_ensemble_albedo(b, g, r, nir, swir1, swir2):
+def get_ensemble_albedo(
+        b: Union[xr.DataArray, np.array],
+        g: Union[xr.DataArray, np.array],
+        r: Union[xr.DataArray, np.array],
+        nir: Union[xr.DataArray, np.array],
+        swir1: Union[xr.DataArray, np.array],
+        swir2: Union[xr.DataArray, np.array]) -> Union[xr.DataArray, np.array]:
     """
     Get an ensemble estimate of all available albedo methods.
 
