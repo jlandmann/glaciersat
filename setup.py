6be89e4a22960324db5e472322cc049f7fc57246
Johannes Landmann <j_landmann@gmx.de>
2021-02-18 19:10:32 +0100

    doc fix

--- setup.py
+++ setup.py
@@ -27,10 +27,7 @@ CLASSIFIERS = [
         # Indicate who your project is intended for
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.9'
     ]
 
 DESCRIPTION = 'glaciersat - monitoring glaciers from space'
